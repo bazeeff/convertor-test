fc86b80a06f234f371b6c9cff26b663d5781cbfa
Ivan Ivanov <artbazik@gmail.com>
2017-03-03 02:10:15 +0300

    created pom.xml

---
+++ src/test/java/SimpleTest.java
@@ -0,0 +1,13 @@
+import org.junit.Test;
+import static junit.framework.TestCase.assertTrue;
+
+/**
+ * Created by artem on 02.03.17.
+ */
+
+public class SimpleTest {
+    @Test
+    public void firstSimpleTest() {
+        assertTrue("Result not equals to 4", 2 * 2 == 4);
+    }
+}
